e76a74f717b933cb6d6e32535d6442fc3cd8fff6
Christopher Gundler <c.gundler@mail.de>
2015-05-07 16:32:03 +0200

    Add customized GUI for "username" and "password"

--- Source/LogIn.php
+++ Source/LogIn.php
@@ -19,17 +19,17 @@ class LogIn extends ThemiSQL {
 
     public function __construct()
     {
-        $output = '<form action="" method="POST"><div class="row"><label class="six columns" for="user">User:</label><input type="text" class="six columns" name="user" id="user" required /></div>';
+        $output = '<form action="" method="POST"><div class="row"><label class="six columns" for="user">' . Config::getPath(['gui', 'username'], 'User:') . '</label><input type="text" class="six columns" name="user" id="user" required /></div>';
         
         if(Config::get('no_password') !== TRUE)
-            $output .= '<div class="row"><label class="six columns" for="pwd">Password:</label><input type="password" class="six columns" name="pwd" id="pwd" required /></div>';
+            $output .= '<div class="row"><label class="six columns" for="pwd">' . Config::getPath(['gui', 'password'], 'Password:') . '</label><input type="password" class="six columns" name="pwd" id="pwd" required /></div>';
 
         $this->setContent(ThemiSQL::FORMAT_CUSTOM, $output . '<div class="row"><input class="button-primary six columns" type="submit" value="' . Config::getPath(['gui', 'logIn'], ThemiSQL::MISSING_GUI) . '"/><input class="six columns" type="reset" value="' . Config::getPath(['gui', 'reset'], ThemiSQL::MISSING_GUI) . '"/></div></form>');
     }
 
     public function getTitle()
     {
-        return 'Log in';
+        return Config::getPath(['gui', 'title'], 'Log in');
     }
 
 }
